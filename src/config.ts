4c22ce26d7875627843064ce0b2b3c88e6bc008f
Planetarium <planetarium1001@163.com>
2025-04-20 16:32:43 +0800

    Update

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
+		enable: true,
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
@@ -52,9 +52,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Planetarium",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	bio: "うーーん……視線を感じます。",
 	links: [
 		//{
 		//	name: "Twitter",
