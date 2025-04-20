55a211d15e546a53f1dd9e3cf557bf53d5dac4ac
Planetarium <planetarium1001@163.com>
2025-04-20 16:35:06 +0800

    Update

--- src/config.ts
+++ src/config.ts
@@ -52,7 +52,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Planetarium",
 	bio: "うーーん……視線を感じます。",
 	links: [
